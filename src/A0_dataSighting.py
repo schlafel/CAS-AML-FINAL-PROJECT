f27a37bf97363a8f7ec2c4de1e3baad69e2e478f
fs <fs@gundp.ch>
2023-03-22 07:04:01 +0100

    Analyse all Files

--- src/A0_dataSighting.py
+++ src/A0_dataSighting.py
@@ -7,6 +7,10 @@ from multiprocessing import Process, Queue
 from tqdm import tqdm
 import numpy as np
 import datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("ggplot")
 def analyze_file(q,q_out,i):
 
     while True:
@@ -14,11 +18,13 @@ def analyze_file(q,q_out,i):
         if item is None:
             print("Process {:d}: Queue is empty".format(i))
             break
-        idx,path = item
-        df_in = pd.read_parquet(path)
+        idx,path,sign,pid = item
+        full_path = os.path.join(ROOT_PATH,RAW_DATA_DIR,path)
+
+        df_in = pd.read_parquet(full_path)
 
         max_length = df_in.shape[0]/543
-        q_out.put((idx,path,max_length))
+        q_out.put((idx,path,max_length,sign,pid))
 
         # finished = q_out.qsize()/q.qsize() *100
         # if finished%10 == 0:
@@ -35,7 +41,7 @@ def start_mp( df_train,   n_processes = 10):
 
     print("Filling Queue...")
     for _,row in tqdm(df_train.iterrows(),total = df_train.shape[0]):
-        q.put((_,os.path.join(ROOT_PATH,RAW_DATA_DIR,row.path)))
+        q.put((_,row.path,row.sign,row.participant_id))
 
 
     procs = []
@@ -68,9 +74,10 @@ def start_mp( df_train,   n_processes = 10):
 
     #now retrieve Queue
     print("Empty Queue....")
-    df_out = pd.DataFrame(np.array(_liout), columns=["idx", "path", "n_frames"])
+    df_out = pd.DataFrame(np.array(_liout), columns=["idx", "path", "n_frames","sign","participant_id"])
     df_out["idx"] = df_out["idx"].map(int)
     df_out["n_frames"] = df_out["n_frames"].map(float).map(int)
+    df_out["participant_id"] = df_out["participant_id"].map(float).map(int)
     return df_out.sort_values(by="idx")
 
 
@@ -80,14 +87,26 @@ def start_mp( df_train,   n_processes = 10):
 
 
 if __name__ == '__main__':
+    statistics_file = os.path.join(ROOT_PATH,DATA_DIR,"sequence_length_dataset.csv")
+    if not os.path.exists(statistics_file):
 
-    df_train = pd.read_csv(os.path.join(ROOT_PATH,RAW_DATA_DIR,"train.csv"))
-    print("done", df_train.shape)
+        df_train = pd.read_csv(os.path.join(ROOT_PATH,RAW_DATA_DIR,"train.csv"))
+        print("done", df_train.shape)
 
-    df_out = start_mp(df_train, n_processes=10)
-    print(df_out["n_frames"].max())
+        df_out = start_mp(df_train, n_processes=10)
+        print(df_out["n_frames"].max())
 
-    df_out.to_csv("sequence_length_dataset.csv",index = False)
+        df_out.to_csv(statistics_file,index = False)
+    else:
+        print("Reading File")
+        df_out = pd.read_csv(statistics_file)
+
+    fig,ax = plt.subplots(1)
+    sns.displot(df_out.n_frames)
+    plt.show()
+
+    print("done")
+
 
 
 
