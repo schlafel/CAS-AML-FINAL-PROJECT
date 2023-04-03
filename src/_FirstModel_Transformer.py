06c7bf40d6f6b1aea149f209f0b5c1c06f28789b
fs <fs@gundp.ch>
2023-04-03 07:59:48 +0200

    Added Transformer Model

---
+++ src/_FirstModel_Transformer.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import os
+import json
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+import pyarrow.parquet as pq
+import numpy as np
+import torch.nn.functional as F
+from tqdm import tqdm
+import pytorch_lightning as pl
+# from sklearn import *
+from torchmetrics.classification import accuracy
+
+from pytorch_lightning.callbacks import ModelCheckpoint,DeviceStatsMonitor
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.tuner import Tuner
+from config import *
+
+from data_models import ASL_DATSET, ASLDataModule, ASLDataModule_Preprocessed
+from models import LSTM_BASELINE_Model, LSTM_Predictor, TransformerPredictor
+
+if __name__ == '__main__':
+    """
+    #1. Load data.
+    
+    #2. Create Model
+    
+    #3. Setting up Callbacks & Trainer
+    
+    #4. Fit the Model
+    """
+
+    MAX_SEQUENCES = 150
+    BATCH_SIZE = 64  #Not optimal as not a perfect power of 2, but maximum that fits in my GPU
+    num_workers = os.cpu_count() // 2  # or 0
+    mod_name = "FIRST_TRANSFORMER_MODEL"
+
+    # ------------ 1. Load data ------------
+    dM = ASLDataModule_Preprocessed(batch_size=BATCH_SIZE,
+                                    max_seq_length=MAX_SEQUENCES,
+                                    num_workers=num_workers)
+    dM.setup()
+    dL = dM.train_dataloader()
+    print(next(iter(dL))["landmarks"].shape)
+    batch = next(iter(dL))["landmarks"]
+    # ------------ 2. Create Model PL------------
+    model = TransformerPredictor(seq_length=MAX_SEQUENCES,
+                                 hidden_size=256)
+
+    # ------------ 3. Create Model Callbacks------------
+    # Model checkpoints
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=os.path.join(ROOT_PATH, "checkpoints"),
+        filename=mod_name + "-{epoch:02d}-{train_accuracy:.2f}",
+        save_top_k=1,
+        monitor="train_loss",
+        verbose=True,
+        mode="min"
+    )
+    # Tensorboard logger
+    tb_logger = TensorBoardLogger(
+        save_dir=os.path.join(ROOT_PATH, "checkpoints","lightning_logs"),
+        name=mod_name,
+        # version=mod_name
+    )
+
+    trainer = pl.Trainer(
+        enable_progress_bar=True,
+        accelerator="gpu",
+        logger=tb_logger,
+        callbacks=[DeviceStatsMonitor(),checkpoint_callback],
+        max_epochs=50,
+        limit_val_batches=0,
+        num_sanity_val_steps=0,
+        profiler=None,#select from None
+    )
+
+    # ------------ 4. Tune Model ------------
+    # tuner = Tuner(trainer)
+    # tuner.scale_batch_size(
+    #     model=model,
+    #     datamodule=dM,
+    #     init_val = 256
+    # )
+    #1024 is optimal...
+
+
+    # ------------ 5. Train Model ------------
+    trainer.fit(
+        model=model,
+        datamodule=dM,
+    )
--- src/models.py
+++ src/models.py
@@ -50,19 +50,22 @@ class LSTM_Predictor(pl.LightningModule):
             num_classes=n_classes
         )
 
-    def forward(self, x, labels = None):
+    def forward(self, x):
         y_hat = self.model(x)
-        loss = 0
-        if labels is not None:
-            loss = self.criterion(y_hat, labels.view(-1))  # need to "flatten" the labels
-        return loss, y_hat
+        return y_hat
 
     def training_step(self, batch, batch_idx):
         landmarks = batch["landmarks"]
         labels = batch["target"]
 
         # forward pass through the model
-        loss, out = self(landmarks, labels)
+        out = self(landmarks)
+
+        #calculate loss
+        loss = 0
+        if labels is not None:
+            loss = self.criterion(out, labels.view(-1))  # need to "flatten" the labels
+
         y_hat = torch.argmax(out, dim=1)
         step_accuracy = self.accuracy(y_hat, labels.view(-1))
 
@@ -75,3 +78,63 @@ class LSTM_Predictor(pl.LightningModule):
 
     def configure_optimizers(self, ):
         return torch.optim.Adam(self.parameters(), lr=LEARNING_RATE)
+
+
+
+class TransformerPredictor(LSTM_Predictor):
+    def __init__(self,seq_length:int =150, hidden_size:int=188,num_classes:int = 250):
+        super().__init__()
+        self.model = TransformerSequenceClassifier(input_dim =seq_length,  hidden_size = hidden_size, num_classes = num_classes)
+        # Define criterion
+        self.criterion = nn.CrossEntropyLoss()
+
+        self.accuracy = accuracy.Accuracy(
+            task="multiclass",
+            num_classes=num_classes
+        )
+
+    def forward(self,x):
+
+        x_padded = torch.nn.functional.pad(x, [0, 68], mode='constant', value=0, )
+        y_hat = self.model(x_padded)
+        return y_hat
+    def validation_step(self, batch, batch_idx):
+        pass
+
+    def configure_optimizers(self, ):
+        return torch.optim.Adam(self.parameters(), lr=LEARNING_RATE)
+
+
+
+
+
+
+
+# Added Transformer Model
+class TransformerSequenceClassifier(nn.Module):
+    def __init__(self, input_dim , num_classes, num_layers=2, hidden_size=256, num_heads=8, dropout=0.1):
+        super().__init__()
+
+        # Transformer layers
+        self.transformer = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(hidden_size, num_heads, hidden_size * 4, dropout),
+            num_layers)
+
+        # Output layer
+        self.output_layer = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, inputs):
+        # Permute the input sequence to match the expected format of the Transformer
+        inputs = inputs.permute(1, 0, 2)
+
+        # Pass the input sequence through the Transformer layers
+        transformed = self.transformer(inputs)
+
+        # Take the mean of the transformed sequence over the time dimension
+        pooled = torch.mean(transformed, dim=0)
+
+        # Pass the pooled sequence through the output layer
+        output = self.output_layer(pooled)
+
+        return output
+
