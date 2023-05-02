19b187b9c8420c78467c0f54cfbf9dfdd2124aac
tgdimas1 <AsadBin.Imtiaz2@swisscom.com>
2023-05-02 17:44:00 +0200

    Fix for TF data loader

--- src/dl_utils.py
+++ src/dl_utils.py
@@ -9,20 +9,27 @@ from torch.utils.data import DataLoader
 
 def get_dataloader(dataset, batch_size=BATCH_SIZE, shuffle=True, dl_framework=DL_FRAMEWORK,num_workers=os.cpu_count()):
     
-    if DL_FRAMEWORK=='TENSORFLOW':
+    if dl_framework=='TENSORFLOW':
         return to_TF_DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
     else:
         return to_PT_DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,num_workers=num_workers)
 
 def to_TF_DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True):
     def preprocess_sample(landmark, target):
-        return tf.constant(landmark), tf.constant(target)
-            
-    tf_data = tf.data.Dataset.from_generator(lambda: dataset, output_types=(tf.float32, tf.int32))    
-        
-    if shuffle:
-        tf_data = tf_data.shuffle(buffer_size=len(dataset))
+        return landmark, target
     
+    indices = np.arange(len(dataset))            
+    
+    def shuffled_dataset_gen():
+        if shuffle:
+            indices = np.arange(len(dataset))
+            np.random.shuffle(indices)
+    
+        for index in indices:
+            yield dataset[index]
+                            
+    tf_data = tf.data.Dataset.from_generator(shuffled_dataset_gen, output_types=(tf.float32, tf.int32))    
+        
     tf_data = tf_data.cache()
     
     tf_data = tf_data.map(preprocess_sample, num_parallel_calls=tf.data.AUTOTUNE)
