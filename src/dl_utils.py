25d52b2b4257caa4af0467fcc7397d1ead14cb8a
fs <fs@gundp.ch>
2023-06-11 21:12:14 +0200

    logging_util

--- src/dl_utils.py
+++ src/dl_utils.py
@@ -204,7 +204,7 @@ def to_PT_DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=o
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
 
-def log_metrics(phase, loss, acc, epoch, lr, writer):
+def log_metrics(writer, log_dict):
     """
     Helper function to log metrics to TensorBoard.
 
@@ -215,9 +215,19 @@ def log_metrics(phase, loss, acc, epoch, lr, writer):
     :param lr: Float, the current learning rate.
     :param writer: TensorBoard writer object.
     """
-    writer.add_scalar(f'Loss/{phase}', loss, epoch + 1)
-    writer.add_scalar(f'Accuracy/{phase}', acc, epoch + 1)
-    print(f"EPOCH {epoch + 1:>3}: {phase} accuracy: {acc:>3.2f}, {phase} Loss: {loss:>9.8f}, LRate {lr:>9.8f} ",
+
+    for key,value in log_dict.items():
+        if key not in ['phase']:
+            writer.add_scalar(f'{key}'.capitalize()+f'/{log_dict["phase"]}', value, log_dict["epoch"] + 1)
+
+    # writer.add_scalar(f'Loss/{phase}', loss, epoch + 1)
+    # writer.add_scalar(f'Accuracy/{phase}', acc, epoch + 1)
+    # for key,value in kwargs:
+    #     writer.add_scalar(f'{key}'.capitalize()+f'/{phase}', value, epoch + 1)
+
+
+    print(f"EPOCH {log_dict['epoch'] + 1:>3}: {log_dict['phase']} accuracy: {log_dict['accuracy']:>3.2f}, "
+          f"{log_dict['phase']} Loss: {log_dict['loss']:>9.8f}, LRate {log_dict['lr']:>9.8f} ",
           flush=True)
 
 
