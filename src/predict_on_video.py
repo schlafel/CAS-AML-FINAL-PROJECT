7187c5e7557331438b0d1a6d3640bc66f2a9aea4
Imtiaz Asad Bin, INI-DNA-DL <AsadBin.Imtiaz@swisscom.com>
2023-07-03 13:54:06 +0500

    Some changes

--- src/predict_on_video.py
+++ src/predict_on_video.py
@@ -65,12 +65,11 @@ def get_top_n_predictions(model_checkpoint, landmarks, n):
 
     sorted_indices = np.argsort(pred)[::-1]
     top_n_indices = sorted_indices[:n]
-    top_n_values = pred[top_n_indices]
 
     return [label_dict_inference[i] for i in top_n_indices]
 
 
-def play_video_with_predictions(video_path, model_checkpoint, num_top_predictions, sign="",  show_mesh=True):
+def play_video_with_predictions(video_path, model_checkpoint, num_top_predictions, sign="", show_mesh=True):
     """
     Plays the video with predicted signs overlay.
 
@@ -133,6 +132,140 @@ def play_video_with_predictions(video_path, model_checkpoint, num_top_prediction
     holistic.close()
 
 
+def resize_and_center_frame(frame, size=(640, 480)):
+    # Get original frame dimensions
+    h, w = frame.shape[:2]
+    desired_w, desired_h = size
+
+    # Calculate the aspect ratio of the frame and the window
+    aspect_ratio_frame = w / h
+    aspect_ratio_window = desired_w / desired_h
+
+    # Determine the new dimensions
+    if aspect_ratio_frame > aspect_ratio_window:
+        new_w = desired_w
+        new_h = int(desired_w / aspect_ratio_frame)
+    else:
+        new_h = desired_h
+        new_w = int(desired_h * aspect_ratio_frame)
+
+    # Resize the frame
+    frame = cv2.resize(frame, (new_w, new_h))
+
+    # Calculate padding (pad equally on two sides)
+    top_pad = (desired_h - new_h) // 2
+    bottom_pad = desired_h - new_h - top_pad
+    left_pad = (desired_w - new_w) // 2
+    right_pad = desired_w - new_w - left_pad
+
+    # Apply padding to the frame
+    frame = cv2.copyMakeBorder(frame, top_pad, bottom_pad, left_pad, right_pad,
+                               cv2.BORDER_CONSTANT, value=[0, 0, 0])
+
+    return frame
+
+
+def resize_and_center_all_frames(frames, size=(640, 480)):
+    # Create an empty list to store the processed frames
+    processed_frames = []
+
+    # Iterate through the frames
+    for frame in frames:
+        # Resize and center the frame
+        frame = resize_and_center_frame(frame, size)
+        # Add the processed frame to the list
+        processed_frames.append(frame)
+
+    return processed_frames
+
+
+def play_random_video_with_predictions(model_checkpoint, num_top_predictions, show_mesh=True):
+    """
+    Plays random video (5 loops) with predicted signs overlay for.
+
+    :param model_checkpoint: str
+        Path to the model checkpoint.
+
+    :param num_top_predictions: int
+        Number of top predictions to overlay on the video.
+
+    :param show_mesh: bool, optional
+        If True, the landmark mesh is drawn on the video. Default is True.
+    """
+
+    quit_loop = False
+
+    model = load_model_from_checkpoint(model_checkpoint)
+    model.eval()
+
+    while True:
+        if quit_loop:
+            break
+
+        video_path = get_random_video()
+        sign = os.path.basename(os.path.dirname(video_path))
+
+        landmarks = get_video_landmarks(video_path)
+        landmarks = standardize(landmarks)
+
+        landmarks = torch.from_numpy(landmarks.astype(np.float32)).to(DEVICE)
+        landmarks = torch.unsqueeze(landmarks, 0)
+        pred = model(landmarks).cpu().detach().numpy()[0]
+
+        sorted_indices = np.argsort(pred)[::-1]
+        top_n_indices = sorted_indices[:num_top_predictions]
+
+        top_n_labels = [label_dict_inference[i] for i in top_n_indices]
+
+        frames = capture_frames(video_path)
+        frames = resize_and_center_all_frames(frames, size=(720, 540))
+
+        mp_holistic = mp.solutions.holistic
+        holistic = mp_holistic.Holistic()
+
+        quit_loop = False
+        loop_counter = 0
+
+        while True:
+            if quit_loop or loop_counter > 2:
+                break
+
+            for i, frame in enumerate(frames):
+
+                if show_mesh and loop_counter == 0:
+                    results = holistic.process(frame)
+
+                    frame = draw_landmarks_on_frame(frame, results)
+
+                cv2.putText(frame, ', '.join(top_n_labels) + f' >> [{sign}]', (3, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
+
+                # Display the frame
+                cv2.imshow('Video Predictor', frame)
+
+                key = cv2.waitKey(30)
+                if key & 0xFF == ord('q'):
+                    quit_loop = True
+                    break
+
+                if key & 0xFF == ord('m'):
+                    show_mesh = not show_mesh
+                    break
+
+                if key & 0xFF == ord('n'):
+                    loop_counter = 1000
+                    break
+
+            if key & 0xFF in [ord('q')]:
+                break
+
+            loop_counter += 1
+
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+    holistic.close()
+
+
 def get_random_video(root_dir='../data/raw/MSASL/Videos/'):
     # Create a generator that will iterate over all mp4 files
     all_videos = (
@@ -151,12 +284,6 @@ def get_random_video(root_dir='../data/raw/MSASL/Videos/'):
 
 
 if __name__ == '__main__':
-
-    ckpt_name = r'TransformerPredictor/2023-06-16 00_18/TransformerPredictor_best_model'
     ckpt_name = r'TransformerPredictor/2023-06-20 07_24/TransformerPredictor_best_model'
 
-    video_path = get_random_video() #"../data/raw/MSASL/Videos/apple/apple_3wq_Jcw0_24_trimmed.mp4"
-    sign = os.path.basename(os.path.dirname(video_path))
-
-    print(video_path)
-    play_video_with_predictions(video_path, ckpt_name, 5, sign, show_mesh=True)
+    play_random_video_with_predictions(ckpt_name, 3, show_mesh=True)
